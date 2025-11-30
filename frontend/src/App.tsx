953f5fbe1591f00db1b4ca6cd9ce838d37fea471
Arsny <arstarn@yandex.ru>
2025-11-30 14:13:42 +0300

    style: Integrate Tailwind CSS and configure shadcn/ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50">
       <h1 className="text-3xl font-bold text-gray-800">
         Metro Dashboard Frontend
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
