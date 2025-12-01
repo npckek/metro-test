90f76ccf6b0a8f5284a77fb26e6a9ec4a01989f4
Arsny <arstarn@yandex.ru>
2025-12-01 18:10:26 +0300

    feat: Added a transition to the showcase

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AdminStationList from '@/components/AdminStationList';
 import { useAuth } from "@/context/AuthContext";
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const AdminDashboardPage: React.FC = () => {
   const { logout } = useAuth();
@@ -11,10 +12,14 @@ const AdminDashboardPage: React.FC = () => {
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-3xl font-bold">Панель Администратора</h1>
 
-        {/* Кнопка выхода */}
-        <Button variant="destructive" onClick={logout}>
-          Выйти
-        </Button>
+        <div className="flex gap-3">
+          <Link to="/">
+            <Button variant="secondary">Перейти на витрину</Button>
+          </Link>
+          <Button variant="destructive" onClick={logout}>
+            Выйти
+          </Button>
+        </div>
       </div>
 
       <AdminStationList />
